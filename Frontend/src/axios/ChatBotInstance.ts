af53120fa270ae225361de4f53747a459b6f7ab6
mendri <marcalmendrighi@gmail.com>
2023-08-30 18:29:22 -0300

    feat: aumentando o tempo de timeout

--- Frontend/src/axios/ChatBotInstance.ts
+++ Frontend/src/axios/ChatBotInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const ChatBotInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 1000
+  timeout: 100000
 })
 
 export default ChatBotInstance
